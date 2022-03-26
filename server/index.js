01505182a78e101c6af7b930622aec7d42c74b9f
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-26 22:45:49 +0400

    connect to db

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 6000;
+const MONGO_URI =
+  "mongodb+srv://admin:mensarius@blog.zoyso.mongodb.net/focus?retryWrites=true&w=majority";
+
+// const MONGO_URI = 'http://localhost'
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Successfully connected to port: ${PORT}`)
+    )
+  )
+  .catch((err) => console.log(err.message));
+
+// mongoose.set('useFindAndModify', false);

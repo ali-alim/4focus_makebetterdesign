88833fa999facfff89cd64f910a71f731c4366e6
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-27 01:18:10 +0400

    create styles file for the  Form component

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {makeStyles} from "./styles"
+
+function Form() {
+    const classes = makeStyles();
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form

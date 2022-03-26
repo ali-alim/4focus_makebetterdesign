ebd294c0bc52eb595cf72f4df48a1bcee78f2ffa
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-27 01:40:27 +0400

    import Form and Posts components to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import todo from "./images/todo.jpg"
 import {makeStyles} from './styles' 
+import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core"
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 function App() {
     // const classes = makeStyles();
   return (
-    <div>
-      Hello, this is manually built App
-    </div>
-  )
+    <Container maxwidth="lg">
+        <AppBar position='static' color='inherit'>
+            <Typography variant="h2" align="center">To-Do Cards</Typography>
+            <img src={todo} alt="todo_cards" height="60" />
+        </AppBar>
+        <Grow in>
+            <Container>
+                <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xs={12} sm={7}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    </Container>
+    )
 }
 
 export default App;

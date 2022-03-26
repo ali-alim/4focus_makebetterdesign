6ee3a7790bb40e1b85c30d051854d81496a0b403
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-26 23:11:06 +0400

    import dotenv package

--- server/index.js
+++ server/index.js
@@ -2,21 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
+const env= dotenv.config()
 const app = express();
 
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 6000;
-const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 7000;
+
   
 
-// const MONGO_URI = 'http://localhost'
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Successfully connected to port: ${PORT}`)

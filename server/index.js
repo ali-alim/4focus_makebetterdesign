41968c31934d366f656b84c699bd2f7e65ee20a6
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-28 01:01:36 +0400

    put postRoutes usage after cors

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const PORT = process.env.PORT || 7000;
 
 const app = express();
 
-app.use('/posts', postRoutes)
+
 
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 app.use(cors());
 
-
+app.use('/posts', postRoutes)
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })

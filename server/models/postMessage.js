6ce9bcc7893f580ae6dae86bda21a1fa0a7b6620
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-29 16:08:31 +0400

    fix

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -12,8 +12,7 @@ const postSchema = mongoose.Schema({
   },
 });
 
-// const PostMessage = mongoose.model("PostMessage", postSchema);
+const PostMessage = mongoose.model("PostMessage", postSchema);
 
-// export default PostMessage;
+export default PostMessage;
 
-module.exports = mongoose.model('PostMessage', postSchema)

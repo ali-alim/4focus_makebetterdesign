eab434bb8a0a6d1469efab90c93954db0fdbc5a1
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-29 15:51:26 +0400

    update functions

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
+import express from 'express'
+
 import PostMessage from "../models/postMessage.js";
+
+const router = express.Router();
 
 export const getPosts = async (req, res) => {
   try {
@@ -10,29 +14,42 @@ export const getPosts = async (req, res) => {
   }
 };
 
+export const getPost = async (req,res) => {
+  const {id} = req.params;
+
+  try{
+    const post = await PostMessage.findById(id);
+    res.status(200).json(post);
+  } catch(err) {
+    res.status(404).json({message: err.message});
+  }
+}  
+
 export const createPost = async (req, res) => {
-    const post = req.body;
-    const newPost = new PostMessage(post);
+    const {title, message, selectedFile, creator, tags} = req.body;
+    // const post = req.body;
+    const newPostMessage = new PostMessage({title, message, selectedFile, creator, tags});
     try{
-        await newPost.save();
-        res.status(201).json(newPost)
+        await newPostMessage.save();
+        res.status(201).json(newPostMessage);
     } catch(err) {
         res.status(409).json({message: err.message})
     }
 };
 
 export const updatePost = async(req,res) => {
-  const {id: _id} = req.params;
-  const post = req.body;
-  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('Post with such id does not exist');
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id,{...post, _id},{new:true}) 
+  const {id} = req.params;
+  const {title, message, selectedFile, creator, tags} = req.body;
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`Post with id ${id} does not exist`);
+  const updatedPost = {creator, title, message, tags, selectedFile, _id:id};
+  await PostMessage.findByIdAndUpdate(_id,updatedPost,{new:true}) 
   res.json(updatedPost)
 }
 
 export const deletePost = async (req, res) => {
   const {id} = req.params;
 
-  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with such id')
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with ${id} id`);
 
   await PostMessage.findByIdAndRemove(id);
 
--- server/index.js
+++ server/index.js
@@ -2,35 +2,32 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-import postRoutes from "./routes/posts.js"
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
 
-const env= dotenv.config()
+const env = dotenv.config();
 const PORT = process.env.PORT || 7000;
 
 const app = express();
 
-
-
-app.use(bodyParser.json({ limit: "20mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use("/posts", postRoutes);
 
 const connectDB = async () => {
-  try{
+  try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Successufully connected to: ${conn.connection.host}`)
-  } catch(err) {
+    console.log(`Successufully connected to: ${conn.connection.host}`);
+  } catch (err) {
     console.log(err);
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 
 connectDB();
 
-app.listen(PORT, () => console.log(`Successufully connected to port: ${PORT}`))
-
+app.listen(PORT, () => console.log(`Successufully connected to port: ${PORT}`));
 
 // mongoose.set('useFindAndModify', false);

88c8397efbec0c11d699c8fce3614aa355dfee76
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-30 12:01:45 +0400

    create Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BrowserRouter,  Switch, Route } from "react-router-dom"
 import useStyles from './styles' 
 import {Container, Grow, Grid} from "@material-ui/core"
 import Posts from './components/Posts/Posts'
@@ -21,18 +22,7 @@ function App() {
     return (
     <Container maxwidth="lg">
         <Navbar />
-        <Grow in>
-            <Container >
-                <Grid className={classes.mainContainer} container justifyContent='space-between' alignItems='stretch' spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId}/>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form currentId={currentId} setCurrentId={setCurrentId} />
-                    </Grid>
-                </Grid>
-            </Container>
-        </Grow>
+        <Home />
     </Container>
     )
 }

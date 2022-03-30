e820cec485f0fee90fb632c2f8de95dae12afcb0
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-30 11:32:46 +0400

    add user case

--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -6,7 +6,7 @@ export default makeStyles((theme) => ({
         borderRadius: 15,
         margin:'30px 0',
         display: 'flex',
-        flexDirection:'row',
+        flexDirection:'column',
         justifyContent: 'space-between',
         alignItems:'center',
         padding: '10px 50px',
